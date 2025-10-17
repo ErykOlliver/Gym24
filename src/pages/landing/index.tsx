4354b22e2ce915a046087d3fcd6f8cd609e86c1b
Eryk Olliver <erykolliver@gmail.com>
2025-10-17 16:25:47 -0300

    estrutura inicial finalizada

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,10 @@
+import Home from "./Home";
+
+export default function Landing() {
+
+    return (
+        <main className="flex flex-col pt-10 bg-black text-white">
+            <Home />
+        </main>
+    )
+}
