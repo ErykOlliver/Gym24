50a7f5c76578dc7ebbc7a98d6d3637e7f0820b3e
Eryk Olliver <erykolliver@gmail.com>
2025-10-20 11:33:26 -0300

    seção Gym Membership finalizada

--- src/pages/landing/MemberShip.tsx
+++ src/pages/landing/MemberShip.tsx
@@ -5,8 +5,8 @@ export default function MemberShip() {
 
     return (
         <section className="font-enconde-sans h-2/4 w-screen snap-start items-center gap-11 px-5 py-15 flex flex-col">
-            <h1 className=" text-3xl font-semibold">Gym membership</h1>
-            <div className="no-scroll grid grid-flow-col overflow-x-auto overflow-y-hidden items-center auto-cols-[275px] auto-rows-[397px] scroll-smooth px-5 py-2.5 w-full gap-4.5 snap-x snap-mandatory">
+            <h1 className="sm:text-5xl text-3xl font-semibold">Gym membership</h1>
+            <div className="sm:grid-cols-2 sm:grid-flow-row sm:overflow-hidden no-scroll grid grid-flow-col overflow-x-auto overflow-y-hidden items-center auto-cols-[275px] auto-rows-[397px] scroll-smooth px-5 py-2.5 w-full gap-4.5 snap-x snap-mandatory">
                 <Pass
                     title="Pass ‘Trial’ "
                     price="$ 0"
