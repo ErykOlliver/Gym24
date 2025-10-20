b0b0d43f519010c90fb25d8355c36cad69205590
Eryk Olliver <erykolliver@gmail.com>
2025-10-20 18:45:39 -0300

    slide finalizado e treiners responsivo

--- src/pages/landing/AboutUs.tsx
+++ src/pages/landing/AboutUs.tsx
@@ -1,22 +1,37 @@
+import Slider from "../../components/Slider";
 
 export default function AboutUs() {
 
 
     return (
-        <section className="h-2/4 w-screen snap-start items-center font-enconde-sans gap-11 px-5 py-15 flex flex-col">
-            <h1 className="sm:text-5xl text-3xl font-semibold">About us</h1>
-            <p className="text-center text-sm flex flex-col gap-5">
-                <span>
-                    Gym24 is a leading fitness center located near Grushevka metro station, offering over 1500 sq.
-                    m of space dedicated to top-of-the-line workout equipment from leading brands such as Hammer Strength,
-                    Life Fitness, and TechnoGym.
-                </span>
-                <span>
-                    Certified trainers at Gym24 provide expert guidance and support to develop personalized workout plans tailored
-                    to each individual's needs and goals.
-                </span>
-            </p>
-            <div className="no-scroll flex overflow-x-auto gap-4.5 px-5 py-2.5 items-center snap-x snap-mandatory">
+        <section className="md:flex-row h-2/4 w-screen snap-start items-end font-enconde-sans gap-11 px-5 pt-15 flex flex-col">
+            <div className="md:items-start md:w-1/2 items-center gap-4.5 flex flex-col">
+                <h1 className=" sm:text-5xl text-3xl font-semibold">About us</h1>
+                <p className="md:text-start text-center text-sm flex flex-col gap-5">
+                    <span>
+                        Gym24 is a leading fitness center located near Grushevka metro station, offering over 1500 sq.
+                        m of space dedicated to top-of-the-line workout equipment from leading brands such as Hammer Strength,
+                        Life Fitness, and TechnoGym.
+                    </span>
+                    <span>
+                        Certified trainers at Gym24 provide expert guidance and support to develop personalized workout plans tailored
+                        to each individual's needs and goals.
+                    </span>
+                    <span className="md:inline hidden ">
+                        Certified trainers at Gym24 provide expert guidance and support to develop personalized workout plans tailored
+                        to each individual's needs and goals.
+                    </span>
+                </p>
+            </div>
+
+            <Slider
+                imgs={[
+                    "/public/photo gym.png",
+                    "/public/photo gym2.png",
+                    "/public/photo gym3.png",
+                    "/public/photo gym4.png",
+                ]} />
+            <div className="md:hidden no-scroll flex overflow-x-auto gap-4.5 px-5 py-2.5 items-center snap-x snap-mandatory">
                 <img src="/public/photo gym.png" alt="" className="sm:w-xl sm:h-96 snap-center w-64 h-52" />
                 <img src="/public/photo gym2.png" alt="" className="sm:w-xl sm:h-96 snap-center w-64 h-52" />
                 <img src="/public/photo gym3.png" alt="" className="sm:w-xl sm:h-96 snap-center w-64 h-52" />
--- src/pages/landing/Treiners.tsx
+++ src/pages/landing/Treiners.tsx
@@ -6,7 +6,7 @@ export default function Treiners() {
     return (
         <section className="font-enconde-sans h-2/4 w-screen snap-start items-center gap-11 px-5 py-15 flex flex-col">
             <h1 className="sm:text-5xl text-3xl font-semibold">Trainers staff</h1>
-            <div className="no-scroll grid grid-flow-col overflow-x-auto overflow-y-hidden items-center auto-cols-[275px] auto-rows-[448px] scroll-smooth px-5 py-2.5 w-full gap-4.5 snap-x snap-mandatory">
+            <div className="md:grid-flow-row md:grid-cols-3 no-scroll grid grid-flow-col overflow-x-auto overflow-y-hidden items-center auto-cols-[275px] auto-rows-[448px] scroll-smooth px-5 py-2.5 w-full gap-4.5 snap-x snap-mandatory">
                 <Treiner name="Victoria Shurpik" photo="/staff/staff1.png" instagram="nolink" twitter="nolink" youtube="nolink" />
                 <Treiner name="Elizabeth Lavrinenko" photo="/staff/staff2.png" instagram="nolink" twitter="nolink" youtube="nolink" />
                 <Treiner name="Ivan Gladkikh" photo="/staff/staff3.png" instagram="nolink" twitter="nolink" youtube="nolink" />
