c70210df9663fa4829e2b1a217cf34b23c69fc96
Eryk Olliver <erykolliver@gmail.com>
2025-10-20 11:42:19 -0300

    Treiners responsiva

--- src/pages/landing/Treiners.tsx
+++ src/pages/landing/Treiners.tsx
@@ -5,7 +5,7 @@ export default function Treiners() {
 
     return (
         <section className="font-enconde-sans h-2/4 w-screen snap-start items-center gap-11 px-5 py-15 flex flex-col">
-            <h1 className=" text-3xl font-semibold">Trainers staff</h1>
+            <h1 className="sm:text-5xl text-3xl font-semibold">Trainers staff</h1>
             <div className="no-scroll grid grid-flow-col overflow-x-auto overflow-y-hidden items-center auto-cols-[275px] auto-rows-[448px] scroll-smooth px-5 py-2.5 w-full gap-4.5 snap-x snap-mandatory">
                 <Treiner name="Victoria Shurpik" photo="/staff/staff1.png" instagram="nolink" twitter="nolink" youtube="nolink" />
                 <Treiner name="Elizabeth Lavrinenko" photo="/staff/staff2.png" instagram="nolink" twitter="nolink" youtube="nolink" />
