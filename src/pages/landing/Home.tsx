42f26d90909996b9d2f58cc8686574d7bed8dd47
Eryk Olliver <erykolliver@gmail.com>
2025-10-20 11:03:35 -0300

    responsividade na home aplicada

--- src/pages/landing/Home.tsx
+++ src/pages/landing/Home.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <section className="Home snap-start font-enconde-sans h-screen w-screen relative bg-no-repeat bg-cover bg-center z-20 flex flex-col">
       <div className="bg-gradient-to-r from-black via-black/70  to-black/30 w-full h-full z-1 absolute"></div>
 
-      <div className="relative px-2 flex flex-col gap-15 z-10 items-center justify-center h-full w-full">
-        <h1 className="text-5xl text-white font-bold text-center">Free trial session with a trainer</h1>
+      <div className="sm:items-start relative px-2 flex flex-col gap-15 z-10 items-center justify-center h-full w-full">
+        <h1 className="sm:text-6xl sm:text-start text-5xl text-white font-bold text-center">Free trial session with a trainer</h1>
         <a href="" className="text-base bg-solarFlare text-black flex items-center justify-center rounded-full font-bold w-44 h-14">Detailed</a>
       </div>
     </section>
